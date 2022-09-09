39a0ac8f530d1db9fd93cd2f8d9a7d2de1cba529
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-09-08 17:19:57 -0700

    Create routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import React from "react";
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import ShoppingCart from "./pages/ShoppingCart";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/cart" element={<ShoppingCart />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
